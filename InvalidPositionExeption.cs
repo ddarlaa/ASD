1360e8e026320cd4cbb4fafdb2df8a838649dace
darla <d.pugina@list.ru>
2026-03-03 18:41:52 +0300

    Обновлен .gitignore

--- InvalidPositionExeption.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ASD;
-
-public class InvalidPositionExeption
-{
-    // Класс исключения для некорректной позиции списка
-    public class InvalidPositionException(string message) : Exception(message) {}
-}
